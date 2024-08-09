2c9c37020c87460cd5f47b4dc955db0124f5bd20
frombunny <rucy011121@gamil.com>
2024-08-09 15:24:10 +0900

    :tada: set up

---
+++ src/test/java/com/hsu/shimpyoo/ShimpyooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsu.shimpyoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShimpyooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
