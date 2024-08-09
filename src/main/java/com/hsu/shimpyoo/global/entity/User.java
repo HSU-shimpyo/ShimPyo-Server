f48c886f7f8c5759df2b740d35e15ce09018ecb3
jungeun-e <lucycy255@gmail.com>
2024-08-09 18:13:18 +0900

    ✨ SignupDto, MyUserDetails 작성

--- src/main/java/com/hsu/shimpyoo/global/entity/User.java
+++ src/main/java/com/hsu/shimpyoo/global/entity/User.java
@@ -1,9 +1,12 @@
 package com.hsu.shimpyoo.global.entity;
 
+import com.hsu.shimpyoo.global.user.dto.SignUpDto;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "USER")
@@ -14,11 +17,11 @@ import java.util.Date;
 public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Long userId; // 사용자 기본 키
+    @Column(name = "id")
+    private Long id; // 사용자 기본 키
 
-    @Column(name = "id", nullable = false, unique = true)
-    private String id; // 아이디
+    @Column(name = "user_id", nullable = false, unique = true)
+    private String userId; // 아이디
 
     @Column(name = "password", nullable = false)
     private String password; // 비밀번호
