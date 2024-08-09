4ad5d024759d92e6912e2ebe2d12d482e22efd37
jungeun-e <lucycy255@gmail.com>
2024-08-09 19:00:35 +0900

    ✨ UserController 작성

---
+++ src/main/java/com/hsu/shimpyoo/global/user/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.hsu.shimpyoo.global.user.controller;
+
+import com.hsu.shimpyoo.global.user.dto.SignUpDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signUp")
+    public ResponseEntity<?> signUp(@RequestBody @Valid SignUpDto dto) {
+        String result = userService.signUp(dto);
+        return ResponseEntity.ok(result);
+    }
+}
