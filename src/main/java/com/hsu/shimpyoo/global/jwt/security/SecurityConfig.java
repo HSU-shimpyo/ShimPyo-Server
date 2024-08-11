bf21225e0e40e50410e2e4c8116c3977f6f7814f
jungeun-e <lucycy255@gmail.com>
2024-08-11 18:16:04 +0900

    ✨ 로그인 기능 구현

--- src/main/java/com/hsu/shimpyoo/global/jwt/security/SecurityConfig.java
+++ src/main/java/com/hsu/shimpyoo/global/jwt/security/SecurityConfig.java
@@ -33,8 +33,4 @@ public class SecurityConfig {
                 .addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-
-
-
-
 }
