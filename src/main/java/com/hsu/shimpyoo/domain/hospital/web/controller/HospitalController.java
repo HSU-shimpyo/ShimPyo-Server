bca9206b7469bd452dd0aee5855840df8cef4ab8
HyeJeong <rucy011121@gmail.com>
2024-08-12 13:29:43 +0900

    :sparkles: FEAT. HospitalController 작성

---
+++ src/main/java/com/hsu/shimpyoo/domain/hospital/web/controller/HospitalController.java
@@ -0,0 +1,22 @@
+package com.hsu.shimpyoo.domain.hospital.web.controller;
+
+import com.hsu.shimpyoo.domain.hospital.service.HospitalService;
+import com.hsu.shimpyoo.domain.hospital.web.dto.HospitalRequestDto;
+import com.hsu.shimpyoo.global.response.CustomAPIResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/hospital")
+@RequiredArgsConstructor
+public class HospitalController {
+    private final HospitalService hospitalService;
+
+    @PostMapping("/getAllHospital")
+    public ResponseEntity<CustomAPIResponse<?>> searchHospitals(@Valid @RequestBody HospitalRequestDto hospitalRequestDto){
+       ResponseEntity<CustomAPIResponse<?>> result= hospitalService.searchHospital(hospitalRequestDto);
+       return result;
+    }
+}
