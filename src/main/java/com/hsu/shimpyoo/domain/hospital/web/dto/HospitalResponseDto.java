8049ab96d21a87d8ec56181e22288471594715c0
HyeJeong <rucy011121@gmail.com>
2024-08-12 14:01:15 +0900

    :sparkles: FEAT. HospitalResponseDto를 사용하여 응답을 예쁘게

---
+++ src/main/java/com/hsu/shimpyoo/domain/hospital/web/dto/HospitalResponseDto.java
@@ -0,0 +1,27 @@
+package com.hsu.shimpyoo.domain.hospital.web.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HospitalResponseDto {
+    // 병원 이름
+    private String hospitalName;
+
+    // 병원 주소
+    private String hospitalAddress;
+
+    // 병원 전화번호
+    private String hospitalPhone;
+
+    // 병원 위치 url
+    private String hospitalUrl;
+
+    // 병원 위도
+    private String latitude;
+
+    // 병원 경도
+    private String longitude;
+}
