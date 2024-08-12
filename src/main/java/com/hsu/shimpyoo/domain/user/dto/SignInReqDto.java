867f1691c37a38316c99f429e7871ff55e23e799
HyeJeong <rucy011121@gmail.com>
2024-08-12 14:58:14 +0900

    Revert "✨ FEAT. 호흡기내과 목록 조회 구현"

--- src/main/java/com/hsu/shimpyoo/domain/user/dto/SignInReqDto.java
+++
@@ -1,9 +0,0 @@
-package com.hsu.shimpyoo.domain.user.dto;
-
-import lombok.Getter;
-
-@Getter
-public class SignInReqDto {
-    private String userId;
-    private String password;
-}
--- src/main/java/com/hsu/shimpyoo/global/response/CustomAPIResponse.java
+++ src/main/java/com/hsu/shimpyoo/global/response/CustomAPIResponse.java
@@ -12,17 +12,16 @@ import lombok.Setter;
 public class CustomAPIResponse<T> {
 
     private int status;
-    private boolean success;
     private T data; // 어떤 형태로 값이 반환될지 모름
     private String message;
 
     // 통신 성공
     public static <T> CustomAPIResponse<T> createSuccess(int status, T data, String message) {
-        return new CustomAPIResponse<T>(status, true, data, message);
+        return new CustomAPIResponse<T>(status, data, message);
     }
 
     // 통신 실패
     public static <T> CustomAPIResponse<T> createFailWithout (int status, String message) {
-        return new CustomAPIResponse<T>(status, false, null, message);
+        return new CustomAPIResponse<T>(status, null, message);
     }
 }
