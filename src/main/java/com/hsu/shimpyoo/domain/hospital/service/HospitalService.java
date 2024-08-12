49ba8f1157134cbc22ae112a75b722b75dbae057
HyeJeong <rucy011121@gmail.com>
2024-08-12 13:17:02 +0900

    :sparkles: FEAT. HospitalService 로직 작성

--- src/main/java/com/hsu/shimpyoo/domain/hospital/service/HospitalService.java
+++ src/main/java/com/hsu/shimpyoo/domain/hospital/service/HospitalService.java
@@ -1,8 +1,9 @@
 package com.hsu.shimpyoo.domain.hospital.service;
 
+import com.hsu.shimpyoo.domain.hospital.web.dto.HospitalRequestDto;
 import com.hsu.shimpyoo.global.response.CustomAPIResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface HospitalService {
-    public ResponseEntity<CustomAPIResponse<?>> searchHospital();
+    public ResponseEntity<CustomAPIResponse<?>> searchHospital(HospitalRequestDto hospitalRequestDto);
 }
