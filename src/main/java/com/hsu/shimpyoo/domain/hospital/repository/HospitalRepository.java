ea2c2d9da256219d3240d5d78091a6eccd71b718
HyeJeong <rucy011121@gmail.com>
2024-08-18 04:47:10 +0900

    :sparkles: FEAT. DataInitializer 클래스 작성

--- src/main/java/com/hsu/shimpyoo/domain/hospital/repository/HospitalRepository.java
+++ src/main/java/com/hsu/shimpyoo/domain/hospital/repository/HospitalRepository.java
@@ -5,12 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
     // 키워드를 포함하는 항목을 찾는다
     List<Hospital> findByHospitalNameContaining(String keyword);
-
-    Optional<Hospital> findByHospitalNameAndHospitalAddress(String hospitalName, String hospitalAddress);
 }
