b263002814c064e11343a05697fb5a2d811d04ed
HyeJeong <rucy011121@gmail.com>
2024-08-18 04:01:40 +0900

    :spakrles: FEAT. 병원 정보 DB에 삽입 코드 작성

--- src/main/java/com/hsu/shimpyoo/domain/hospital/web/dto/HospitalRequestDto.java
+++ src/main/java/com/hsu/shimpyoo/domain/hospital/web/dto/HospitalRequestDto.java
@@ -1,25 +1,12 @@
 package com.hsu.shimpyoo.domain.hospital.web.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class HospitalRequestDto {
-    // 호흡기내과를 조회 및 페이징 처리
-
-    // 현재 페이지 번호 -> 기본 값을 1로 설정
-    private int page = 1;
-
-    // 한 페이지에 출력할 목록의 수 -> 기본 값을 10으로 설정
-    private int size = 10;
-
-    public HospitalRequestDto() {}
-
-    public HospitalRequestDto(int page, int size) {
-        this.page = page;
-        this.size = size;
-    }
+    // 키워드
+    private String keyword;
 
 }
