3ae3310b1e54d51c536b50d84928c8e59d4994ff
jungeun-e <lucycy255@gmail.com>
2024-08-18 14:12:02 +0900

    🎨 SignUpDto 수정

--- src/main/java/com/hsu/shimpyoo/domain/user/dto/SignUpDto.java
+++ src/main/java/com/hsu/shimpyoo/domain/user/dto/SignUpDto.java
@@ -12,11 +12,19 @@ import java.util.Date;
 @Setter
 @NoArgsConstructor
 public class SignUpDto {
-    @NotNull(message = "아이디를 적어주세요.")
+    @NotNull(message = "이름을 입력해주세요.")
+    private String name;
+
+    @NotNull(message = "아이디를 입력해주세요.")
     private String loginId;
-    @NotNull(message = "비밀번호를 적어주세요.")
+
+    @NotNull(message = "비밀번호를 입력해주세요.")
     private String password;
-    @NotNull(message = "생년월일을 적어주세요.")
+
+    @NotNull(message = "생년월일을 입력해주세요.")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
     private Date birth;
+
+    @NotNull(message = "최대호기량을 입력해주세요.")
+    private Long pef;
 }
