a3ccd0f7ef46cc4be50ccf1fe5c423718508b2fa
HyeJeong <rucy011121@gmail.com>
2024-08-27 00:58:46 +0900

    :sparkles: FEAT. 호흡 파일 업로드 컨트롤러 작성

--- src/main/java/com/hsu/shimpyoo/domain/breathing/controller/BreathingController.java
+++ src/main/java/com/hsu/shimpyoo/domain/breathing/controller/BreathingController.java
@@ -1,6 +1,7 @@
 package com.hsu.shimpyoo.domain.breathing.controller;
 
 import com.hsu.shimpyoo.domain.breathing.dto.BreathingRequestDto;
+import com.hsu.shimpyoo.domain.breathing.dto.BreathingUploadRequestDto;
 import com.hsu.shimpyoo.domain.breathing.service.BreathingService;
 import com.hsu.shimpyoo.domain.user.entity.User;
 import com.hsu.shimpyoo.domain.user.repository.UserRepository;
@@ -10,7 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +24,21 @@ public class BreathingController {
     private final BreathingService breathingService;
     private final AuthenticationUserUtils authenticationUserUtils;
     private final UserRepository userRepository;
+
+    // 녹음 파일 업로드
+    @PostMapping("/uploadFile")
+    public ResponseEntity<CustomAPIResponse<?>> uploadFile(
+            @RequestPart("date") String date,
+            @RequestPart("firstFile") MultipartFile firstFile,
+            @RequestPart("secondFile") MultipartFile secondFile,
+            @RequestPart("thirdFile") MultipartFile thirdFile) throws IOException {
+
+        // 오류로 인해 RequestPart로 받은 후, DTO로 변환
+        BreathingUploadRequestDto breathingUploadRequestDto = new BreathingUploadRequestDto(date, firstFile, secondFile, thirdFile);
+
+        ResponseEntity<CustomAPIResponse<?>> result=breathingService.uploadBreathing(breathingUploadRequestDto);
+        return result;
+    }
 
     // 오늘의 쉼 결과
     @PostMapping("/todayResult")
