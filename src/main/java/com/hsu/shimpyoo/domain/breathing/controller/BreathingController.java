71643ad626da10f00c8ff4beddbdc2f5ae234e43
jungeun-e <lucycy255@gmail.com>
2024-08-26 16:29:40 +0900

    ✨ 지난 7일간 내역 조회 기능 구현

--- src/main/java/com/hsu/shimpyoo/domain/breathing/controller/BreathingController.java
+++ src/main/java/com/hsu/shimpyoo/domain/breathing/controller/BreathingController.java
@@ -7,9 +7,11 @@ import com.hsu.shimpyoo.domain.user.repository.UserRepository;
 import com.hsu.shimpyoo.global.response.CustomAPIResponse;
 import com.hsu.shimpyoo.global.security.jwt.util.AuthenticationUserUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -30,5 +32,19 @@ public class BreathingController {
                 .orElseThrow(() -> new UsernameNotFoundException("해당 사용자가 존재하지 않습니다."));
 
         return breathingService.calculateBreathingResult(dto, user);
+    }
+
+    // 오늘을 기준으로 지난 7일간의 쉼 결과 조회
+    @GetMapping("/todayWeekly")
+    public ResponseEntity<CustomAPIResponse<?>> getTodayBreathingWeekly() {
+        // 현재 로그인된 사용자 정보 가져오기
+        String loginId = authenticationUserUtils.getCurrentUserId();
+        User user = userRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자가 존재하지 않습니다."));
+
+        // 지난 7일간의 breathingRate 가져오기
+        List<Map<String, Object>> weeklyBreathingRates = breathingService.getWeeklyBreathingRates(user);
+
+        return ResponseEntity.ok(CustomAPIResponse.createSuccess(200, weeklyBreathingRates, "지난 7일간의 최대호기량 조회에 성공했습니다."));
     }
 }
