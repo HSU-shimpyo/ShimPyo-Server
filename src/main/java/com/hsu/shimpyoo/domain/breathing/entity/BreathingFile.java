08442c1440b31fa89f8ba79220c763b379f94cae
HyeJeong <rucy011121@gmail.com>
2024-08-26 23:48:10 +0900

    :sparkles: FEAT. BreathingFile 엔터티 생성

---
+++ src/main/java/com/hsu/shimpyoo/domain/breathing/entity/BreathingFile.java
@@ -0,0 +1,32 @@
+package com.hsu.shimpyoo.domain.breathing.entity;
+
+import com.hsu.shimpyoo.domain.user.entity.User;
+import com.hsu.shimpyoo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "BREATHING_FILE")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class BreathingFile extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "breathing_file_id")
+    private Long breathingFileId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId; // 사용자 기본키
+
+    @Column(name="first_url")
+    private String firstUrl; // 첫번째 파일의 url
+
+    @Column(name="second_url")
+    private String secondUrl; // 두번째 파일의 url
+
+    @Column(name="third_url")
+    private String thirdUrl; // 세번째 파일의 url
+}
