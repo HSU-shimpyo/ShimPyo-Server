8f454939d9e6b7e041cff968ca3a89adba211178
jungeun-e <lucycy255@gmail.com>
2024-08-29 14:47:55 +0900

    ✨ DailyPef 엔티티 생성

---
+++ src/main/java/com/hsu/shimpyoo/domain/breathing/entity/DailyPef.java
@@ -0,0 +1,32 @@
+package com.hsu.shimpyoo.domain.breathing.entity;
+
+import com.hsu.shimpyoo.domain.user.entity.User;
+import com.hsu.shimpyoo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "DAILY_PEF")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class DailyPef extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "daily_pef_id")
+    private Long dailyPefId; // 일별 측정 기본 키
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId; // 사용자 기본 키
+
+    @Column(name = "pef")
+    private Float pef; // 최대호기량
+
+    @Column(name = "state")
+    private Enum state; // 상태
+
+    @Column(name = "week_day")
+    private Enum weekDay; // 요알
+}
