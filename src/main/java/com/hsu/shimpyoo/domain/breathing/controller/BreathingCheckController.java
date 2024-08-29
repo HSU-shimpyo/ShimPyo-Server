b4fc6a4cfef39eb091d21546d8b0f73d08fa7a87
frombunny <rucy011121@gamil.com>
2024-08-29 14:54:55 +0900

    :recycle: CHORE. 플라스크 통신에 따른 기존 코드 수정

--- src/main/java/com/hsu/shimpyoo/domain/breathing/controller/BreathingCheckController.java
+++ src/main/java/com/hsu/shimpyoo/domain/breathing/controller/BreathingCheckController.java
@@ -1,6 +1,8 @@
 package com.hsu.shimpyoo.domain.breathing.controller;
 
+import com.hsu.shimpyoo.domain.breathing.dto.BreathingFlaskDto;
 import com.hsu.shimpyoo.domain.breathing.dto.BreathingUploadRequestDto;
+import com.hsu.shimpyoo.domain.breathing.entity.BreathingFile;
 import com.hsu.shimpyoo.domain.breathing.service.BreathingCheckServiceImpl;
 import com.hsu.shimpyoo.global.response.CustomAPIResponse;
 import lombok.RequiredArgsConstructor;
@@ -29,8 +31,16 @@ public class BreathingCheckController {
 
         // 오류로 인해 RequestPart로 받은 후, DTO로 변환
         BreathingUploadRequestDto breathingUploadRequestDto = new BreathingUploadRequestDto(date, firstFile, secondFile, thirdFile);
+        BreathingFile breathingFile = breathingCheckServiceImpl.uploadBreathing(breathingUploadRequestDto);
 
-        ResponseEntity<CustomAPIResponse<?>> result=breathingCheckServiceImpl.uploadBreathing(breathingUploadRequestDto);
-        return result;
+        BreathingFlaskDto breathingFlaskDto=new BreathingFlaskDto();
+
+        // 플라스크 서버에 보낼 파일 url
+        breathingFlaskDto.setFirstFile(breathingFile.getFirstUrl());
+        breathingFlaskDto.setSecondFile(breathingFile.getSecondUrl());
+        breathingFlaskDto.setThirdFile(breathingFile.getThirdUrl());
+
+        // 임의로 넣은 ResponseEntity 코드임
+        return ResponseEntity.ok(CustomAPIResponse.createSuccess(200, null, "성공했습니다."));
     }
 }
