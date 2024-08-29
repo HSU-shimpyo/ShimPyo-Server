cc6a0a3d47c7bcd64e5592b7d15682957c8d192d
frombunny <rucy011121@gamil.com>
2024-08-29 16:20:05 +0900

    :sparkles: FEAT. 플라스크 통신을 이용하여 파이썬에서 호흡 수치를 받아옴

--- src/main/java/com/hsu/shimpyoo/ShimpyooApplication.java
+++ src/main/java/com/hsu/shimpyoo/ShimpyooApplication.java
@@ -2,7 +2,9 @@ package com.hsu.shimpyoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +12,11 @@ public class ShimpyooApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShimpyooApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
