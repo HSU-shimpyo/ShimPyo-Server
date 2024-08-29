7e7bb40af8b8d958f11ada34a2383eeb3d20d3df
frombunny <rucy011121@gmail.com>
2024-08-29 16:20:05 +0900

    :sparkles: FEAT. 플라스크 통신을 이용하여 파이썬에서 호흡 수치를 받아옴

--- src/main/java/com/hsu/shimpyoo/domain/breathing/service/BreathingCheckService.java
+++ src/main/java/com/hsu/shimpyoo/domain/breathing/service/BreathingCheckService.java
@@ -1,11 +1,15 @@
 package com.hsu.shimpyoo.domain.breathing.service;
 
+import com.hsu.shimpyoo.domain.breathing.dto.BreathingFlaskDto;
 import com.hsu.shimpyoo.domain.breathing.dto.BreathingUploadRequestDto;
 import com.hsu.shimpyoo.domain.breathing.entity.BreathingFile;
+import com.hsu.shimpyoo.global.response.CustomAPIResponse;
+import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 
 
 public interface BreathingCheckService {
     BreathingFile uploadBreathing(BreathingUploadRequestDto breathingUploadRequestDto) throws IOException;
+    ResponseEntity<CustomAPIResponse<?>> analyzeBreathing(BreathingFlaskDto breathingFlaskDto) throws IOException;
 }
