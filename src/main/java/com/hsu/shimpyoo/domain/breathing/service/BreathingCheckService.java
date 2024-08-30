1b5b04cf1e29b728c9d62642bed8e23e5d9396c7
frombunny <rucy011121@gmail.com>
2024-08-31 02:38:29 +0900

    :recycle: REFACTOR. anlyzeBreathing의 반환 형식 수정

--- src/main/java/com/hsu/shimpyoo/domain/breathing/service/BreathingCheckService.java
+++ src/main/java/com/hsu/shimpyoo/domain/breathing/service/BreathingCheckService.java
@@ -2,6 +2,7 @@ package com.hsu.shimpyoo.domain.breathing.service;
 
 import com.hsu.shimpyoo.domain.breathing.web.dto.BreathingFlaskRequestDto;
 import com.hsu.shimpyoo.domain.breathing.entity.BreathingFile;
+import com.hsu.shimpyoo.domain.breathing.web.dto.BreathingPefDto;
 import com.hsu.shimpyoo.domain.breathing.web.dto.BreathingUploadRequestDto;
 import com.hsu.shimpyoo.global.response.CustomAPIResponse;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,6 @@ import java.io.IOException;
 
 public interface BreathingCheckService {
     BreathingFile uploadBreathing(BreathingUploadRequestDto breathingUploadRequestDto) throws IOException;
-    ResponseEntity<CustomAPIResponse<?>> analyzeBreathing
+    BreathingPefDto analyzeBreathing
             (BreathingFlaskRequestDto breathingFlaskRequestDto, Long breathingFileId) throws IOException;
 }
