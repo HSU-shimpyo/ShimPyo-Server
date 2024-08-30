f3f40257c5abd76bb1bb4a76c96ad43d3a563389
frombunny <rucy011121@gamil.com>
2024-08-31 04:26:40 +0900

    :recycle: REFACTOR. 측정 로직 수정

--- src/main/java/com/hsu/shimpyoo/domain/breathing/service/BreathingCheckService.java
+++ src/main/java/com/hsu/shimpyoo/domain/breathing/service/BreathingCheckService.java
@@ -1,17 +1,15 @@
 package com.hsu.shimpyoo.domain.breathing.service;
 
+import com.hsu.shimpyoo.domain.breathing.entity.Breathing;
 import com.hsu.shimpyoo.domain.breathing.web.dto.BreathingFlaskRequestDto;
 import com.hsu.shimpyoo.domain.breathing.entity.BreathingFile;
-import com.hsu.shimpyoo.domain.breathing.web.dto.BreathingPefDto;
 import com.hsu.shimpyoo.domain.breathing.web.dto.BreathingUploadRequestDto;
-import com.hsu.shimpyoo.global.response.CustomAPIResponse;
-import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 
 
 public interface BreathingCheckService {
     BreathingFile uploadBreathing(BreathingUploadRequestDto breathingUploadRequestDto) throws IOException;
-    BreathingPefDto analyzeBreathing
+    Breathing analyzeBreathing
             (BreathingFlaskRequestDto breathingFlaskRequestDto, Long breathingFileId) throws IOException;
 }
--- src/main/java/com/hsu/shimpyoo/domain/breathing/web/controller/BreathingController.java
+++ src/main/java/com/hsu/shimpyoo/domain/breathing/web/controller/BreathingController.java
@@ -22,17 +22,17 @@ public class BreathingController {
     private final AuthenticationUserUtils authenticationUserUtils;
     private final UserRepository userRepository;
 
-    // 오늘의 쉼 결과
-    @PostMapping("/today/result")
-    public CustomAPIResponse<Map<String, Object>> getTodayBreathingResult(
-            @RequestBody BreathingPefDto dto) {
-        // 현재 로그인된 사용자 정보 가져오기
-        String loginId = authenticationUserUtils.getCurrentUserId();
-        User user = userRepository.findByLoginId(loginId)
-                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자가 존재하지 않습니다."));
-
-        return breathingService.calculateBreathingResult(dto, user);
-    }
+//    // 오늘의 쉼 결과
+//    @PostMapping("/today/result")
+//    public CustomAPIResponse<Map<String, Object>> getTodayBreathingResult(
+//            @RequestBody BreathingPefDto dto) {
+//        // 현재 로그인된 사용자 정보 가져오기
+//        String loginId = authenticationUserUtils.getCurrentUserId();
+//        User user = userRepository.findByLoginId(loginId)
+//                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자가 존재하지 않습니다."));
+//
+//        return breathingService.calculateBreathingResult(dto, user);
+//    }
 
     // 오늘을 기준으로 지난 7일간의 쉼 결과 조회
     @GetMapping("/today/weekly")
