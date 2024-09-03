ec824c6a19914b046f36430102f902138d34155b
frombunny <rucy011121@gmail.com>
2024-09-03 19:15:03 +0900

    :sparkles: FEAT. 재측정 기능 구현

--- src/main/java/com/hsu/shimpyoo/domain/breathing/service/BreathingCheckService.java
+++ src/main/java/com/hsu/shimpyoo/domain/breathing/service/BreathingCheckService.java
@@ -4,6 +4,8 @@ import com.hsu.shimpyoo.domain.breathing.entity.Breathing;
 import com.hsu.shimpyoo.domain.breathing.web.dto.BreathingFlaskRequestDto;
 import com.hsu.shimpyoo.domain.breathing.entity.BreathingFile;
 import com.hsu.shimpyoo.domain.breathing.web.dto.BreathingUploadRequestDto;
+import com.hsu.shimpyoo.global.response.CustomAPIResponse;
+import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 
@@ -12,4 +14,5 @@ public interface BreathingCheckService {
     BreathingFile uploadBreathing(BreathingUploadRequestDto breathingUploadRequestDto) throws IOException;
     Breathing analyzeBreathing
             (BreathingFlaskRequestDto breathingFlaskRequestDto, Long breathingFileId) throws IOException;
+    void deleteBreathing();
 }
