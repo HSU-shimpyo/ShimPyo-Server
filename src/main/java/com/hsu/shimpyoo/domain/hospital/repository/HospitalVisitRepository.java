f7ff707b08c330e68340e0eef7b0e685d3296ff9
frombunny <rucy011121@gamil.com>
2024-09-04 16:50:36 +0900

    :sparkles: FEAT. 병원 방문까지 남은 시간 조회 기능 구현

--- src/main/java/com/hsu/shimpyoo/domain/hospital/repository/HospitalVisitRepository.java
+++ src/main/java/com/hsu/shimpyoo/domain/hospital/repository/HospitalVisitRepository.java
@@ -5,6 +5,7 @@ import com.hsu.shimpyoo.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,7 +14,7 @@ public interface HospitalVisitRepository extends JpaRepository<HospitalVisit, Lo
     List<HospitalVisit> findByUserId(User userId);
 
 
-    // 사용자 ID로 방문 일정을 조회하고, 방문 시간을 오름차순으로 정렬하여 가장 빠른 일정 반환
-    Optional<HospitalVisit> findFirstByUserIdOrderByVisitTimeAsc(Long userId);
+    // 사용자 ID로 미래의 방문 일정을 조회하고, 방문 시간을 오름차순으로 정렬하여 가장 빠른 일정 반환
+    Optional<HospitalVisit> findFirstByUserIdAndVisitTimeAfterOrderByVisitTimeAsc(User userId, LocalDateTime now);
 
 }
