c4c26d61c1191a61eef41b190bb5e161e9117312
frombunny <rucy011121@gmail.com>
2024-09-05 09:37:35 +0900

    :sparkles: FEAT. ChatResponseDto 작성

---
+++ src/main/java/com/hsu/shimpyoo/domain/chatbot/dto/ChatResponseDto.java
@@ -0,0 +1,21 @@
+package com.hsu.shimpyoo.domain.chatbot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatResponseDto {
+    private List<ChatChoice> chatChoices;
+
+    public static class ChatChoice{ // 여러 개의 응답이 있을 수 있고, 그 중에서 선택할 수 있음
+        private int index;
+        private ChatMessageDto chatMessageDto;
+    }
+}
