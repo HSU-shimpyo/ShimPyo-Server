69daa2eed0bd69ffd3a4df46e3682cfff3b7efb7
frombunny <rucy011121@gamil.com>
2024-09-07 18:21:32 +0900

    :sparkles: FEAT. ChatRepository 작성

---
+++ src/main/java/com/hsu/shimpyoo/domain/chatbot/repository/ChatRepository.java
@@ -0,0 +1,10 @@
+package com.hsu.shimpyoo.domain.chatbot.repository;
+
+import com.hsu.shimpyoo.domain.chatbot.entity.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+
+}
