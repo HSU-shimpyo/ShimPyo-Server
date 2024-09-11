2e6ca76e4946ecbef8f651d9a6f2771da315aa57
frombunny <rucy011121@gmail.com>
2024-09-12 01:15:41 +0900

    :sparkles: FEAT. 채팅방 제목 검색 기능 구현(#73)

--- src/main/java/com/hsu/shimpyoo/domain/chatbot/service/ChatRoomServiceImpl.java
+++ src/main/java/com/hsu/shimpyoo/domain/chatbot/service/ChatRoomServiceImpl.java
@@ -84,7 +84,7 @@ public class ChatRoomServiceImpl implements ChatRoomService {
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
-    // 채팅방 목록 조회
+    // 모든 채팅방 목록 조회
     @Override
     public ResponseEntity<CustomAPIResponse<?>> getAllChatRooms() {
         // 현재 인증된 사용자의 로그인 아이디를 가져옴 (getName은 loginId를 가져오는 것)
@@ -163,7 +163,51 @@ public class ChatRoomServiceImpl implements ChatRoomService {
             chatListDtos.add(chatListDto);
         }
 
-        CustomAPIResponse<List<ChatListDto>> response = CustomAPIResponse.createSuccess(200, chatListDtos, "채팅방 목록 조회에 성공하였습니다.");
+        CustomAPIResponse<List<ChatListDto>> response = CustomAPIResponse.createSuccess(200, chatListDtos, "대화 내역 조회에 성공하였습니다.");
+        return ResponseEntity.ok(response);
+    }
+
+
+    // 채팅방 검색 기능
+    @Override
+    public ResponseEntity<CustomAPIResponse<?>> getChatRoomByKeyword(String keyword) {
+        // 현재 인증된 사용자의 로그인 아이디를 가져옴 (getName은 loginId를 가져오는 것)
+        String loginId= SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // 사용자 존재 여부 확인
+        Optional<User> isExistUser=userRepository.findByLoginId(loginId);
+        if(isExistUser.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"존재하지 않는 사용자입니다.");
+        }
+
+        // 사용자 아이디와 키워드로 채팅방 검색
+        List<ChatRoom> chatRoomList= chatRoomRepository.findChatRoomByChatTitleContainingAndUserId(keyword, isExistUser.get());
+
+        // 채팅방 정보 목록을 담을 리스트 생성
+        List<ChatRoomListDto> chatRoomListDtos = new ArrayList<>();
+
+        // 각 채팅방에 대해 마지막 메시지 조회
+        for (ChatRoom chatRoom : chatRoomList) {
+            // 마지막 메시지를 ChatRepository를 통해 조회
+            Optional<Chat> lastChat = chatRepository.findTopByUserIdAndChatRoomIdOrderByCreatedAtDesc(isExistUser.get(), chatRoom);
+
+            // dto의 마지막 메시지와 마지막 시간 정보 추가
+            String lastChatMessage = lastChat.map(Chat::getContent).orElse("메시지가 존재하지 않습니다.");
+            String lastChatAt = lastChat.map(chat -> chat.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm")))
+                    .orElse("시간이 존재하지 않습니다.");
+
+            // ChatRoomListDto에 값을 채워서 리스트에 추가
+            ChatRoomListDto chatRoomListDto = new ChatRoomListDto(
+                    chatRoom.getChatRoomId(),  // 채팅방 ID
+                    chatRoom.getChatTitle(),  // 채팅방 제목
+                    lastChatMessage,  // 마지막 메시지
+                    lastChatAt  // 마지막 대화 시간
+            );
+
+            chatRoomListDtos.add(chatRoomListDto);
+        }
+
+        CustomAPIResponse<List<ChatRoomListDto>> response = CustomAPIResponse.createSuccess(200, chatRoomListDtos, "채팅방 목록 조회에 성공하였습니다.");
         return ResponseEntity.ok(response);
     }
 }
