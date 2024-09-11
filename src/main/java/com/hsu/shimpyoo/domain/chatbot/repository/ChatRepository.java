121ea2f18032da86a4bc5428a9d4d0b6e5af05b9
frombunny <rucy011121@gamil.com>
2024-09-12 00:31:53 +0900

    :sparkles: FEAT. 채팅방 목록 조회 기능 구현(#74)

--- src/main/java/com/hsu/shimpyoo/domain/chatbot/repository/ChatRepository.java
+++ src/main/java/com/hsu/shimpyoo/domain/chatbot/repository/ChatRepository.java
@@ -1,9 +1,15 @@
 package com.hsu.shimpyoo.domain.chatbot.repository;
 
 import com.hsu.shimpyoo.domain.chatbot.entity.Chat;
+import com.hsu.shimpyoo.domain.chatbot.entity.ChatRoom;
+import com.hsu.shimpyoo.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+    // 사용자 기본키와 채팅방 기본키로 검색하여 가장 최근의 메시지 반환
+    Optional<Chat> findTopByUserIdAndChatRoomIdOrderByCreatedAtDesc(User user, ChatRoom chatRoom);
 }
